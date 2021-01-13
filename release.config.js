4f57e2df64e235d0345c53b25520758688a502cc
Ibrahimmaga <ibrahim.maga@yahoo.fr>
2021-01-12 23:59:21 -0800

    feat: semantic feature

--- release.config.js
+++ release.config.js
@@ -1,8 +1,6 @@
-module.exports = {
-    branches: "master",
-    repositoryUrl: "https://github.com/Ibrahimmaga/myreact-app",
-    plugins: ["@semantic-release/commit-analyzer", 
-              "@semantic-release/release-notes-generator",
-              "@semantic-release/github"
-    ]
-};
+export const branches = "master";
+export const repositoryUrl = "https://github.com/Ibrahimmaga/myreact-app";
+export const plugins = ["@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+];
