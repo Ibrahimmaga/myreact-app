e01057cebbf910a11b123be0e9e3ffa799b2d247
Ibrahimmaga <ibrahim.maga@yahoo.fr>
2021-01-24 13:08:45 -0800

    feat: some feature

---
+++ release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/Ibrahimmaga/myreact-app",
+    plugins: ["@semantic-release/commit-analyzer",
+              "@semantic-release/release-notes-generator",
+              "@semantic-release/npm",
+              "@semantic-release/github"]
+};
